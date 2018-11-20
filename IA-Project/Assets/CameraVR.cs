11631dadaf2e85582268be7d133edafc9cddebec
Abiel Villegas <villegaspro64@gmail.com>
2018-11-19 23:17:30 -0800

    Primera analogía, 25%

---
+++ IA-Project/Assets/CameraVR.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class CameraVR : MonoBehaviour {
+    [SerializeField] private float m_RenderScale = 1f;
+	// Use this for initialization
+	void Start () {
+        UnityEngine.XR.XRSettings.eyeTextureResolutionScale = m_RenderScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ IA-Project/Assets/Scripts/AudioRespuestas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRespuestas : MonoBehaviour {
+    public GameObject clip;
+    //List<GameObject> clips = new List<GameObject>();
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void Reproducir() {
+        var clips = GameObject.FindGameObjectsWithTag("audio_1");
+        clip.GetComponent<AudioSource>().Play();
+        //for (int i = 0; i < clip.Count; i++)
+        //{
+
+        //}
+        //clip.GetComponent<AudioSource>(Random.Range(0, 3).Play();
+    }
+
+    
+}
