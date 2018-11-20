01bc502583fa234cb400e56da1b1d7e8a29ff4fb
Abiel Villegas <villegaspro64@gmail.com>
2018-11-20 00:08:41 -0800

    Update escena 1

--- IA-Project/Assets/Scripts/AudioRespuestas.cs
+++ IA-Project/Assets/Scripts/AudioRespuestas.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioRespuestas : MonoBehaviour {
     public GameObject clip;
+    public GameObject[] clips;
     
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,22 @@ public class AudioRespuestas : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Reproducir();
     }
 
     public void Reproducir() {
-        clip = GameObject.FindGameObjectWithTag("audio_1");
-        clip.GetComponent<AudioSource>().Play();
+        System.Random random = new System.Random();
+
+        //clip = GameObject.FindGameObjectWithTag("audio_1");
+        clips = GameObject.FindGameObjectsWithTag("audio_1");
+
+        int random_audio = random.Next(0, clips.Length-1);
+        clips[random_audio].GetComponent<AudioSource>().Play();
+        //clip.GetComponent<AudioSource>().Play();
+    }
+
+    public void Score() {
+
     }
 
     
